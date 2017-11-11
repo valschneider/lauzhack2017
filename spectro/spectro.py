05ea7012bafa28a1cbb791b802c3a0e567571123
Valentin Schneider <valentin.schneider42@gmail.com>
2017-11-11 20:18:31 +0000

    spectro: Clear keyboard first

--- spectro/spectro.py
+++ spectro/spectro.py
@@ -6,11 +6,10 @@ class Spectro(object):
         self.phys_kbd = phys_kbd
 
     def update_phys_kbd(self):
+        self.phys_kbd.set_all_colour(Colours(0, 0, 0))
         for row, freq in enumerate(self.freqs):
             keys = self.phys_kbd.layout["rows"][row]
 
             keys_to_lit = min(freq / 10, len(keys))
             for key in keys[0:keys_to_lit]:
                 self.phys_kbd.set_key_colour(key, Colours(0, 100, 100))
-            for key in keys[keys_to_lit:-1]:
-                self.phys_kbd.set_key_colour(key, Colours(0, 0, 0))
