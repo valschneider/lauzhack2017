599905bcf44bb9c86a9b1901a24a56523068c6d9
Valentin Schneider <valentin.schneider42@gmail.com>
2017-11-11 18:08:29 +0000

    demos: Discard red_enter and add row_setter

---
+++ demos/row_setter.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+basepath = os.path.dirname(os.path.abspath(__file__))
+basepath = os.path.abspath(os.path.join(basepath, os.pardir))
+
+sys.path.append(basepath)
+from utils import PhysicalKeyboard
+from utils import Colours
+
+kbd = PhysicalKeyboard()
+
+for idx, row in kbd.layout["rows"].iteritems():
+    for key in row:
+        if (idx % 3) == 0:
+            kbd.set_key_colour(kbd.keys[key], Colours(100, 0, 0))
+        elif (idx + 1 % 3) == 0:
+            kbd.set_key_colour(kbd.keys[key], Colours(0, 100, 0))
+        elif (idx + 2 % 3) == 0:
+            kbd.set_key_colour(kbd.keys[key], Colours(0, 0, 100))
