e0095f83c4ceb679bd861a01f40da0c52da1cb3a
lmayencourt <louis.mayencourt@gmail.ch>
2017-11-11 21:37:10 +0100

    add super super smooth transition
    Signed-off-by: lmayencourt <louis.mayencourt@gmail.ch>

--- follow_me.py
+++ follow_me.py
@@ -6,6 +6,8 @@ from utils.abstract_keyboard_display import *
 from utils import PhysicalKeyboard
 
 kbd = PhysicalKeyboard()
+#ak = AbstractKeyboard()
+#kbd = AbstractKeyboardDisplay(ak)
 
 def generate_sequence():
     seq = ''.join(random.choice(string.ascii_lowercase) for _ in range(10))
