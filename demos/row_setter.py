0d2ba1dc58a2b67eaf4b5b90143ade8743632f91
Valentin Schneider <valentin.schneider42@gmail.com>
2017-11-11 18:34:23 +0000

    demos/row_setter: Make rows easier to distinguish

--- demos/row_setter.py
+++ demos/row_setter.py
@@ -14,11 +14,11 @@ for idx, row in kbd.layout["rows"].iteritems():
     for col, key in enumerate(row):
         if (idx % 2) == 0:
             kbd.set_key_colour(kbd.keys[key]["keycode"], Colours(
-                2*col, 3*col, 4*col
+                100, 0, 0
             ))
         else:
             kbd.set_key_colour(kbd.keys[key]["keycode"], Colours(
-                4*col, 3*col, 2*col
+                0, 0, 100
             ))
 
 raw_input("Press ENTER to end test")
