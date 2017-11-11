02ab0129060f3f6a639aabbc9836d52f028767c1
lmayencourt <louis.mayencourt@gmail.ch>
2017-11-11 15:40:34 +0100

    abstractKeyboardDisplay use abstractkeyboard data

--- utils/abstract_keyboard_display.py
+++ utils/abstract_keyboard_display.py
@@ -2,21 +2,24 @@
 '''
 
 from graphics import *
+#from webcolors import *
+from abstract_keyboard import *
 
 class AbstractKeyboardDisplay:
 
-    def __init__(self):
+    def __init__(self, abstract_keyboard):
         height = 200
         width = 500
+        self.abs_key = abstract_keyboard
         self.size = 20
         self.win = GraphWin('Abstract Keyboard viewer', width, height)
 
     def display(self):
-        for x in range(0,21):
-            for y in range(0,5):
+        for x in range(0,self.abs_key.length - 1):
+            for y in range(0,self.abs_key.width - 1):
                 key = Rectangle(Point(50+self.size*x,50+ self.size*y), Point(50+self.size*x+self.size, 50+self.size*y+self.size))
-                #rgb_to_name((0, 0, 0))
-                key.setFill('midnightblue')
+                #key.setFill('midnightblue')
+                key.setFill('#%02x%02x%02x' % (self.abs_key.keys[y][x].colors.r, self.abs_key.keys[y][x].colors.g, self.abs_key.keys[y][x].colors.b))
                 key.draw(self.win)
 
     def wait_user_interaction(self):
@@ -26,7 +29,14 @@ class AbstractKeyboardDisplay:
         self.win.close()
 
 def main():
-    disp = AbstractKeyboardDisplay()
+    # init a the absract keyboard with data
+    abs_key = AbstractKeyboard()
+    for x in xrange(abs_key.length):
+        for y in xrange(abs_key.width):
+            abs_key.keys[y][x].colors = Colors(x*256/abs_key.length,y*256/abs_key.width,(x+y)*2)
+
+    # create a disply with the abstract display
+    disp = AbstractKeyboardDisplay(abs_key)
     disp.display()
     disp.wait_user_interaction()
 
