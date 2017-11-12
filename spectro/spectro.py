c1d9dfa671c0a8e7e3538e675298bd324b0e45cd
Valentin Schneider <valentin.schneider42@gmail.com>
2017-11-12 02:33:19 +0000

    spectro: What is life ? Baby don't hurt me

--- spectro/spectro.py
+++ spectro/spectro.py
@@ -6,16 +6,29 @@ from time import sleep, time
 
 from utils import Colours
 
+red = Colours(100, 0, 0)
+yellow = Colours(100, 100, 0)
+green = Colours(0, 100, 0)
+cyan = Colours(0, 100, 100)
+blue = Colours(0, 0, 100)
+purple = Colours(100, 0, 100)
+
 class Spectro(object):
     def __init__(self, phys_kbd, harmonics=6):
         self.freqs = [0 for i in xrange(harmonics)]
         self.phys_kbd = phys_kbd
 
+    def get_master_volume(self):
+        volume = int(subprocess.check_output('adjust_get_current_system_volume_vista_plus'))
+        return volume
+
     def play(self, sound_file):
         sampling_rate, data = wavfile.read(sound_file)
         data = data / (2.**15)
         data = abs(data)
-        data = data * 500
+        # In average, more than half the keyboard is list
+        data = data * (50. / data.mean())
+        #data = data * 500
 
         sound_duration = data.shape[0] / sampling_rate
 
@@ -32,7 +45,9 @@ class Spectro(object):
             sleep(wait_time)
             now = time() - start
 
-            res = data[int(prev * sampling_rate):int(now * sampling_rate)].mean()
+            ratio = self.get_master_volume() / 15.
+
+            res = data[int(prev * sampling_rate):int(now * sampling_rate)].mean() * ratio
 
             for i in xrange(len(self.freqs)):
                 self.freqs[i] = int(res)
@@ -41,12 +56,23 @@ class Spectro(object):
             prev = now
 
     def update_phys_kbd(self):
-        #self.phys_kbd.set_all_colour(Colours(0, 0, 0))
         for row, freq in enumerate(self.freqs):
             keys = self.phys_kbd.layout["rows"][row]
 
             keys_to_lit = min(freq / 10, len(keys))
-            for key in keys[0:keys_to_lit]:
-                self.phys_kbd.set_key_colour(key, Colours(0, 100, 100))
+            for idx, key in enumerate(keys[0:keys_to_lit]):
+                depth = (100 * idx) / len(keys)
+                if depth <= 20:
+                    colour = blue
+                elif depth <= 40:
+                    colour = cyan
+                elif depth <= 60:
+                    colour = green
+                elif depth <= 80:
+                    colour = yellow
+                elif depth <= 100:
+                    colour = red
+
+                self.phys_kbd.set_key_colour(key, colour)
             for key in keys[keys_to_lit:]:
                 self.phys_kbd.set_key_colour(key, Colours(0, 0, 0))
